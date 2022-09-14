f4a4c713ba794113645bb6a407ec7bb204e3cb84
Max Boog <maxboog@MacBook-Air-van-Max.local>
2022-09-14 19:48:44 +0200

    change font, prototype changes

--- assets/js/customNav.js
+++ assets/js/customNav.js
@@ -23,9 +23,9 @@ class CustomNav extends HTMLElement {
         >
           <div class="offcanvas-header">
             <div></div>
-            <h3 class="offcanvas-title" id="offcanvasNavbarLabel">
+            <!-- <h3 class="offcanvas-title" id="offcanvasNavbarLabel">
               Lazy Reflex Complex
-            </h3>
+            </h3> -->
             <button
               type="button"
               class="btn-close"
@@ -36,17 +36,17 @@ class CustomNav extends HTMLElement {
           <div class="offcanvas-body">
             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link active fw-bold" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/about.html">About</a>
+                <a class="nav-link fw-bold" href="/about.html">About</a>
               </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="/releases.html">Releases</a>
+                  <a class="nav-link fw-bold" href="/releases.html">Releases</a>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                   <a class="nav-link disabled" href="#">Vinyl Releases</a>
-                </li>
+                </li> -->
                 <li class="nav-item">
                   <a class="nav-link nav-link-margin" href="/releases/LRC01.html">LRC01 // 543ff, Jeroen Search</a>
                 </li>
@@ -56,14 +56,20 @@ class CustomNav extends HTMLElement {
                 <li class="nav-item">
                   <a class="nav-link nav-link-margin" href="#">LRC03 // TBD</a>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                   <a class="nav-link disabled" href="#">Ambient Releases</a>
-                </li>
+                </li> -->
                 <li class="nav-item">
                   <a class="nav-link nav-link-margin" href="/releases/LRC-AMBIENT01.html">LRCAMBIENT01 // 543ff</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link nav-link-margin" href="#">LRCAMBIENT02 // TBD</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link fw-bold" href="#">Podcasts</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link nav-link-margin" href="/releases/LRC01.html">LRCPODCAST01 // 543ff</a>
                 </li>
             </div>
         </div>
